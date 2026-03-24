a74b95c72bc8adb95c681bf4eb836f89731b2d9d
Saniya64-24 <saniya6480@gmail.com>
2026-03-24 23:27:53 +0530

    commit 8

--- experiment_automation/tests/API/users/test_user.py
+++ experiment_automation/tests/API/users/test_user.py
@@ -1,20 +1,24 @@
 import pytest
+import uuid
 from src.clients.user_cleint import create_users
 
 
 @pytest.mark.parametrize(
-    "name,email,password,role,age",
+    "name,password,role,age",
     [
-        ("Alice123", "alice1123@example.com", "Password123!", "user", 22),
-        ("Bob123", "bob113@example.com", "Password123!", "user", 25),
-        ("Charlie123", "charlie113@example.com", "Password123!", "admin", 30),
+        ("Alice123", "Password123!", "user", 22),
+        ("Bob123", "Password123!", "user", 25),
+        ("Charlie123", "Password123!", "admin", 30),
     ]
 )
-def test_create_multiple_users(admin_token, name, email, password, role, age):
+def test_create_multiple_users(admin_token, name, password, role, age):
+
+    # Generate random email
+    random_email = f"{name.lower()}_{uuid.uuid4().hex[:6]}@example.com"
 
     payload = {
         "name": name,
-        "email": email,
+        "email": random_email,
         "password": password,
         "role": role,
         "age": age
@@ -22,13 +26,11 @@ def test_create_multiple_users(admin_token, name, email, password, role, age):
 
     response = create_users(payload, admin_token)
 
-    assert response.status_code == 201 # (409)conflict becuse already created .if not replace it with 201
+    assert response.status_code == 201
 
     response_data = response.json()["data"]
 
     assert response_data["name"] == name
-    assert response_data["email"] == email
+    assert response_data["email"] == random_email
     assert response_data["role"] == role
     assert response_data["age"] == age
-
-    
